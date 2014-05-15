572c9c734f21e23e2a65485fd8dffb0b380e264c
HIDEKUNI Kajita <hide.nba@gmail.com>
2014-05-15 09:33:31 +0900

    forward port 4243(docker remote api port)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require 'net/ssh'
 include SpecInfra::Helper::Ssh
 include SpecInfra::Helper::DetectOS
 
+require 'docker'
+require 'tapp'
+
 RSpec.configure do |c|
   if ENV['ASK_SUDO_PASSWORD']
     require 'highline/import'
@@ -14,32 +17,13 @@ RSpec.configure do |c|
   end
   c.before :all do
     block = self.class.metadata[:example_group_block]
-    if RUBY_VERSION.start_with?('1.8')
-      file = block.to_s.match(/.*@(.*):[0-9]+>/)[1]
-    else
-      file = block.source_location.first
-    end
+    file = block.source_location.first
     host  = File.basename(Pathname.new(file).dirname)
     if c.host != host
       c.ssh.close if c.ssh
       c.host  = host
       options = Net::SSH::Config.for(c.host)
       user    = options[:user] || Etc.getlogin
-      vagrant_up = `vagrant up default`
-      config = `vagrant ssh-config default`
-      if config != ''
-        config.each_line do |line|
-          if match = /HostName (.*)/.match(line)
-            host = match[1]
-          elsif  match = /User (.*)/.match(line)
-            user = match[1]
-          elsif match = /IdentityFile (.*)/.match(line)
-            options[:keys] =  [match[1].gsub(/"/,'')]
-          elsif match = /Port (.*)/.match(line)
-            options[:port] = match[1]
-          end
-        end
-      end
       c.ssh   = Net::SSH.start(host, user, options)
     end
   end
